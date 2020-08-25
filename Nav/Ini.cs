9de9bc7cf5fa7488ce6ee93222f79e6e74c4849e
Cr33zz <pawel.creez.kubiak@gmail.com>
2020-08-25 12:49:12 -0400

    Added extra functionalities to ini class.

--- Nav/Ini.cs
+++ Nav/Ini.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Ini
@@ -12,12 +13,14 @@ namespace Ini
         public string path;
 
         [DllImport("kernel32")]
-        private static extern long WritePrivateProfileString(string section,
-            string key, string val, string filePath);
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
         [DllImport("kernel32")]
-        private static extern int GetPrivateProfileString(string section,
-                 string key, string def, StringBuilder retVal,
-            int size, string filePath);
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+        [DllImport("kernel32")]
+        private static extern uint GetPrivateProfileSectionNames(IntPtr pszReturnBuffer, uint nSize, string lpFileName);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileSection(string lpAppName, byte[] lpszReturnBuffer, int nSize, string lpFileName);
+
 
         /// <summary>
         /// INIFile Constructor.
@@ -51,11 +54,38 @@ namespace Ini
         public string IniReadValue(string Section, string Key)
         {
             StringBuilder temp = new StringBuilder(255);
-            int i = GetPrivateProfileString(Section, Key, "", temp,
-                                            255, this.path);
+            int i = GetPrivateProfileString(Section, Key, "", temp, 255, this.path);
 
             //remove comments
             return temp.ToString().Split(';')[0];
+        }
+
+        public string[] IniSectionNames()
+        {
+            uint MAX_BUFFER = 32767;
+            IntPtr pReturnedString = Marshal.AllocCoTaskMem((int)MAX_BUFFER);
+            uint bytesReturned = GetPrivateProfileSectionNames(pReturnedString, MAX_BUFFER, this.path);
+            if (bytesReturned == 0)
+                return null;
+            string local = Marshal.PtrToStringAnsi(pReturnedString, (int)bytesReturned).ToString();
+            Marshal.FreeCoTaskMem(pReturnedString);
+            //use of Substring below removes terminating null for split
+            return local.Substring(0, local.Length - 1).Split('\0');
+        }
+
+        public Tuple<string, string>[] IniSectionData(string Section)
+        {
+            int MAX_BUFFER = 32767;
+            byte[] buffer = new byte[MAX_BUFFER];
+
+            GetPrivateProfileSection(Section, buffer, MAX_BUFFER, path);
+            
+            List<Tuple<string, string>> result = new List<Tuple<string, string>>();
+
+            foreach (string entry in Encoding.ASCII.GetString(buffer).Trim('\0').Split('\0'))
+                result.Add(Tuple.Create(entry.Substring(0, entry.IndexOf("=")), entry.Substring(entry.IndexOf("=") + 1)));
+
+            return result.ToArray();
         }
     }
 }
